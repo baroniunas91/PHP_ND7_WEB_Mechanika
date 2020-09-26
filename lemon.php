da0f059a8f8efa0bbb8d884f6e70ef9685702615
Edgaras <baroniunas@gmail.com>
2020-09-26 13:57:55 +0300

    fourth task done

---
+++ lemon.php
@@ -0,0 +1,13 @@
+<?php
+echo '4) ----------------------------------------------------------------------------------------------------';
+echo '<br>';
+echo '<br>';
+// Sukurkite du puslapius lemon.php ir orange.php. Jų fonus nuspalvinkite atitinkamom spalvom. 
+// Į lemon.php puslapį įdėkite kodą, kuris naršyklę visada peradresuotų į puslapį orange.php. 
+// Pademonstruokite veikimą.
+header('Location: http://localhost/BIT/ND7_WEB_Mechanika/orange.php');
+die();
+?>
+
+<body style="background-color: yellow">
+</body>
---
+++ orange.php
@@ -0,0 +1,11 @@
+<?php
+echo '4) ----------------------------------------------------------------------------------------------------';
+echo '<br>';
+echo '<br>';
+// Sukurkite du puslapius lemon.php ir orange.php. Jų fonus nuspalvinkite atitinkamom spalvom. 
+// Į lemon.php puslapį įdėkite kodą, kuris naršyklę visada peradresuotų į puslapį orange.php. 
+// Pademonstruokite veikimą.
+?>
+
+<body style="background-color: orange">
+</body>
